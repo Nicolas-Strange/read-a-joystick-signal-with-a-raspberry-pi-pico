43b9562cdc05958f054f48cafa2471d7c1f57ba6
wataiso <wataiso@gmail.com>
2023-02-10 11:52:42 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Read a Joystick signal with a Raspberry Pi Pico
+
+![https://diy-home.org/](misc/joystick_bb.png)
+
+Welcome to the "Read a Joystick signal with a Raspberry Pi Pico" 
+repository!
+
+You can find the full tutorial [here](https://diy-home.org/2023/02/10/read-a-joystick-signal-with-a-raspberry-pi-pico/)
+- how to build your device
+- code explanation
+- how to setup your Raspberry Pi Pico
+
+![https://diy-home.org/](misc/banner.png)
---
+++ main.py
@@ -0,0 +1,26 @@
+from joystick import JoystickReader
+
+
+class Main:
+    """ main class that will handle the loop """
+
+    def __init__(self):
+        """
+        init function
+        """
+        self._joystick = JoystickReader()
+
+    def run(self) -> None:
+        """
+        core function to do whatever you want with the Joystick values
+        For each iteration the joystick value will be read and display in the terminal
+        """
+        self._joystick.calibrate()
+        while True:
+            x, y, z = self._joystick.read()
+            print(f"x: {x} -- y: {y} -- z: {z}")
+
+
+if __name__ == '__main__':
+    run = Main()
+    run.run()
